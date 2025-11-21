4e9685b8a9b5015a06f5d9088ea812645837f630
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-21 10:33:28 +0530

    updated project folder with new files

--- Threejs-Project/src/main.js
+++ Threejs-Project/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -9,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -18,8 +19,17 @@ const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
   window.requestAnimationFrame(animate);
+  controls.update(); // Update controls each frame
   renderer.render(scene, camera);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
