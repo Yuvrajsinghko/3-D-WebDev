7a66c91623f19e2a50d4359d8d2a3dc8209f118b
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-21 10:20:57 +0530

    updated project folder with new files

--- Threejs-Project/src/main.js
+++ Threejs-Project/src/main.js
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -7,13 +9,13 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
-const canvas = document.querySelector('canvas');
-const renderer = new THREE.WebGLRenderer({canvas});
+const canvas = document.querySelector("canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 function animate() {
