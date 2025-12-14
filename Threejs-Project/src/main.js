cce7781a7eeb15f924ec7fb3e2576f69b1e6764f
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-14 23:32:04 +0530

    updated main.js

--- Threejs-Project/src/main.js
+++ Threejs-Project/src/main.js
@@ -1,7 +1,49 @@
+// import * as THREE from "three";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+// const canvas = document.querySelector("canvas");
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// window.addEventListener("resize", () => {
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+// });
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.autoRotate = true;
+// controls.autoRotateSpeed = 2.0;
+// controls.dampingFactor = 0.05;
+// function animate() {
+//   window.requestAnimationFrame(animate);
+//   controls.update(); // Update controls each frame
+//   renderer.render(scene, camera);
+// //   cube.rotation.x += 0.01;
+// //   cube.rotation.y += 0.01;
+// }
+
+// animate();
+
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -10,12 +52,13 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
-const canvas = document.querySelector("canvas");
+
+const canvas = document.querySelector("canvas#draw") || document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -25,14 +68,12 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
-const controls = new OrbitControls(camera, renderer.domElement);
-
 function animate() {
-  window.requestAnimationFrame(animate);
-  controls.update(); // Update controls each frame
-  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
 
 animate();
+
